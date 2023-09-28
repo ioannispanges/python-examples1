3ca0a75945eeb71f2310be4ad64210abbe54215e
yannis <pagges@kic.uoi.gr>
2023-09-28 17:46:05 +0300

    rectangle_area.py

---
+++ mathima 22/2_epanalipsi/rectangle_area.py
@@ -0,0 +1,10 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+
+length = 5
+width = 3
+
+area_of_rectangle = calculate_area(length, width)
+print("The area of the rectangle is :", area_of_rectangle)
