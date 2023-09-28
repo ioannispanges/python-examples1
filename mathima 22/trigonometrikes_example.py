f02a2cf0295cbd45d03376b74064268f2ea4f1a7
yannis <pagges@kic.uoi.gr>
2023-09-28 17:23:15 +0300

    radians2_example.py

---
+++ mathima 22/trigonometrikes_example.py
@@ -0,0 +1,10 @@
+import math
+
+angle_degrees = 45
+angle_radians = math.radians(angle_degrees)
+
+sin_value = math.sin(angle_radians)
+cos_value = math.cos(angle_radians)
+
+print(f"Sin of {angle_degrees} degrees : {sin_value}")
+print(f"Cos of {angle_degrees} degrees : {cos_value}")
