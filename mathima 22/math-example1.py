7076554bf39a3cbb16af2d79d0f6ccbdb7e84296
yannis <pagges@kic.uoi.gr>
2023-09-28 16:25:22 +0300

    math_example.py

---
+++ mathima 22/math-example1.py
@@ -0,0 +1,8 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+
+print("The square root of", number, "is", square_root)
+
---
+++ mathima 22/math_example2.py
@@ -0,0 +1,7 @@
+import math
+number = int(input("enter the number to square up: "))
+
+number
+square_root = math.sqrt(number)
+
+print("The square root of", number, "is", square_root)
