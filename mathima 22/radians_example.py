4823743c8b66edea3f78506ef3aa313d96777c4a
yannis <pagges@kic.uoi.gr>
2023-09-28 17:15:43 +0300

    radians_example.py

---
+++ mathima 22/radians_example.py
@@ -0,0 +1,10 @@
+import math
+
+degrees = 45
+radians = math.radians(degrees)
+print(f"{degrees} degrees is equal to {radians:.2f} radians")
+
+radians = math.pi / 4
+degrees = math.degrees(radians)
+
+print(f"{radians:.2f} radians is equal to {degrees} degrees")
