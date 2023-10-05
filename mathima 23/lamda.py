343d53acd773db1619b8615fa03bea9ebbcd20c1
yannis <pagges@kic.uoi.gr>
2023-10-05 18:32:50 +0300

    closures.py

--- mathima 23/lamda.py
+++ mathima 23/lamda.py
@@ -1,3 +1,3 @@
 students = [("Alice", 22), ("Bob", 22), ("Charlie", 22)]
-students_sorted = sorted(students, key=lambda student: students[1])
+students_sorted = sorted(students, key=lambda student: students[0])
 print(students_sorted)
