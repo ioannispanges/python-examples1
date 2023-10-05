4c56d57c2feb6788759470fbe15b0c029f9022c1
yannis <pagges@kic.uoi.gr>
2023-10-05 18:42:04 +0300

    closures.py

---
+++ mathima 23/decorators.py
@@ -0,0 +1,15 @@
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before function is called")
+        func()
+        print("Something is happening after the function is called")
+
+    return wrapper()
+
+
+@my_decorator
+def say_hello():
+    print("Hello!!!")
+
+
+say_hello
