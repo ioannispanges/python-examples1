58a6584f877162207b097a2a7f480f7961528b80
yannis <pagges@kic.uoi.gr>
2023-10-05 17:27:16 +0300

    new

---
+++ mathima 22/2_epanalipsi/by_default.py
@@ -0,0 +1,6 @@
+def greet (name, greeting= "Hello"):
+    return f"{greeting}, {name}!"
+
+
+result = greet("Doe")
+print(result)
---
+++ mathima 22/2_epanalipsi/whitespace.py
@@ -0,0 +1,11 @@
+def calculate_sym(a, b):
+    result = a + b
+    return result
+
+
+x = 5
+y = 10
+
+if x > 0:
+    z = calculate_sym(x, y)
+    print(f"The sum of {x} and {y} is {z}")
