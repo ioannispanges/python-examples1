ae9374b8c26158368a5bde333b75d8cc3f735abd
yannis <pagges@kic.uoi.gr>
2023-10-12 18:33:40 +0300

    example .py

---
+++ mathima 24/example2.py
@@ -0,0 +1,7 @@
+number = int(input('Έισάγετε έναν αριθμο :'))
+
+if number != 0:
+    result = 1 / number
+    print("Ο αντίστροφος του αριθμού", number, "ειναι:", result)
+else:
+    print(" Ο αριθμος πρέπει να ειναι διάφορος του μηδενός")
