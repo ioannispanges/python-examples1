e7285a59328e4ad063d210e69c8f4070990a3dee
yannis <pagges@kic.uoi.gr>
2023-10-12 18:43:54 +0300

    zerodivisionError + ValueError .py

---
+++ mathima 24/example4.py
@@ -0,0 +1,9 @@
+try:
+    numerator = int(input("Eisagete enan arithmiti: "))
+    denominator = int(input("Eisagete ena paranomasti: "))
+    result = numerator / denominator
+    print("Apotelesma", result)
+except ZeroDivisionError:
+    print("Error: Den epitrepetai i diairesi me to miden")
+except ValueError:
+    print("Error: Parakaloume eisagete egkires sto paranomasti")
