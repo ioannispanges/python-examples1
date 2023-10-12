07c924593a965b88ffc89eee651fd7b2e78b4a50
yannis <pagges@kic.uoi.gr>
2023-10-12 17:28:15 +0300

    shuffle.py

---
+++ mathima 24/random_module.py
@@ -0,0 +1,8 @@
+import random
+
+random_num = random.randint(1, 11)
+print(f"Random number : {random_num}")
+
+my_list = [1, 2, 3, 4, 5, 6]
+random.shuffle(my_list)
+print(f"Shuffled list: {my_list}")
