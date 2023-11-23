5e16dc54065d4dc6988cb5b146bc93af7b953e2d
yannis <pagges@kic.uoi.gr>
2023-11-23 18:06:30 +0200

    epanalipsi_person-other

--- mathima 30/poli_me_dedomena.py
+++ mathima 30/poli_me_dedomena.py
@@ -1,7 +1,16 @@
-def print_value(value):
-    print(value)
+import epanalipsi_person as person
+
+# from epanalipsi_person import Person
+
+person2 = person.Person("Alex", 35)
+
+print(person.Person.emfanizw(self=person2))
 
 
-print(10)
-print("Hello!!!!")
-print(True)
+# def print_value(value):
+#     print(value)
+# #
+# #
+# # print(10)
+# # print("Hello!!!!")
+# # print(True)
