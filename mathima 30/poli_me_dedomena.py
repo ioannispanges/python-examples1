300d3a9701becaf68c442a7a78aef434d46d1eba
yannis <pagges@kic.uoi.gr>
2023-11-23 16:39:54 +0200

    polimorfismos me dedomena

---
+++ mathima 30/poli_me_dedomena.py
@@ -0,0 +1,7 @@
+def print_value(value):
+    print(value)
+
+
+print(10)
+print("Hello!!!!")
+print(True)
--- mathima 30/polimorfismos.py
+++ mathima 30/polimorfismos.py
@@ -1,8 +1,10 @@
+# goniki klassi######################
 class Animal:
     def speak(self):
         print("I am an animal")
 
 
+############################
 class Dog(Animal):
     def speak(self):
         print("Woof")
