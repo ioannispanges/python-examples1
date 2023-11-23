9b82b0752608add80af568b509d140c1592b2e88
yannis <pagges@kic.uoi.gr>
2023-11-23 17:28:03 +0200

    epanalipsi_person

---
+++ mathima 30/epanalipsi_person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def emfanizw(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+
+person1 = Person("John Doe", 25)
+person1.emfanizw()
