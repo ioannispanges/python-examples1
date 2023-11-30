d77611aa9eaecb90bc99d50706041fd012deef5c
yannis <pagges@kic.uoi.gr>
2023-11-30 13:25:19 +0200

    quiz

--- mathima 30/epanalipsi_quiz.py
+++ mathima 30/epanalipsi_quiz.py
@@ -8,29 +8,47 @@ class Quiz:
 
     def take_quiz(self):
         score = 0
-        for question, correct_answer in self.questions.items():
-            user_answer = input(f"{question} (Enter your answer: ")
-            if user_answer.lower() == correct_answer.lower():
-                print("Correct!")
-                score = 1
+        total_questions = len(self.questions)
+
+        if not self.questions:
+            print("No questions added to the quiz yet")
+            return
+
+        print("Available questions:")
+        for idx, question in enumerate(self.questions, start=1):
+            print(f"{idx}. {question}")
+
+        try:
+            question_number = int(input("Choose a question number to answer: "))
+            if 1 <= question_number <= total_questions:
+                selected_question = list(self.questions.keys())[question_number - 1]
+                user_answer = input(f"{selected_question} (Enter your answer): ")
+
+                if user_answer.lower() == self.questions[selected_question].lower():
+                    print("Correct!")
+                    score += 1
+                else:
+                    print(f"Incorrect! The correct answer is {self.questions[selected_question]}")
             else:
-                print(f"Incorrect! The correct answer is{correct_answer}")
+                print("Invalid question number. Please choose a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-        print(f"You scored {score}/ {len(self.questions)} in the quiz")
+        print(f"You scored {score}/{total_questions} in the quiz")
 
-    def display_question(self):
+    def display_questions(self):
         if self.questions:
             print("Quiz Questions:")
             for question in self.questions:
                 print(f" {question}")
-            else:
-                print("No question added to the quiz yet")
+        else:
+            print("No questions added to the quiz yet")
 
 
-quiz = Quiz
+quiz = Quiz()
 quiz.add_question("What is the capital of France?", "Paris")
-quiz.add_question("What is 2+2", "4")
-quiz.add_question("Who wrote 'Romeo and Juliet?", "Shakespeare")
+quiz.add_question("What is 2+2?", "4")
+quiz.add_question("Who wrote 'Romeo and Juliet'?", "Shakespeare")
 
-quiz.display_question()
+quiz.display_questions()
 quiz.take_quiz()
