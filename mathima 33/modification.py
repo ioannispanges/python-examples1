f7176accff1ee77c8c27a6a8be1b369f0d1ffd7f
yannis <pagges@kic.uoi.gr>
2023-12-14 18:22:01 +0200

    6

---
+++ mathima 33/modification.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import time
+
+current_file = r'C:\Users\evdokimos\Desktop\python\mathima 33\services\price.xls'
+new_file = r'C:\Users\evdokimos\Desktop\python\mathima 33\latest\price.xls'
+
+attr_current = os.stat(current_file)
+attr_new = os.stat(new_file)
+
+print(f"Modification time of current file: {time.ctime(attr_current.st_mtime)}")
+print(f'Modification time of new file:{time.ctime(attr_new.st_mtime)}')
+
+if attr_current.st_mtime < attr_new.st_mtime:
+    shutil.copy(new_file, current_file)
+    print(f"{new_file} has been compied to {current_file} due to its more recent modification time")
+else:
+    print("No action was take as the modification time of the new file")
