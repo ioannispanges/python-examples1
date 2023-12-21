a42292774fd6b6eb89226afb188ebebfba031ec2
yannis <pagges@kic.uoi.gr>
2023-12-21 17:04:03 +0200

    button1

--- mathima 34/button.py
+++ mathima 34/button.py
@@ -1,13 +1,11 @@
 import tkinter as tk
 
-
 def on_button_click():
     print("Button clicked")
 
-
 window = tk.Tk()
 window.title("Event Handling Example")
-
+window.geometry("400x200")
 button = tk.Button(window, text="Click me", command=on_button_click)
 button.pack()
 
