03b5d1f0b72dcab096decde7b895d11afc8a50a8
yannis <pagges@kic.uoi.gr>
2023-12-21 17:15:47 +0200

    button1

---
+++ mathima 34/button1.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+def on_button_click(event):
+    print(f"Button clicked at({event.x}, {event.y})")
+
+
+window = tk.Tk()
+window.title("Event Handling Example")
+window.geometry("400x200")
+
+button = tk.Button(window, text="Click me")
+button.bind("<Button-1>", on_button_click)
+button.pack()
+
+window.mainloop()
