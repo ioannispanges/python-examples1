1e5351fc97eba3438552f195359b9134f9ffae8f
yannis <pagges@kic.uoi.gr>
2024-01-04 16:16:33 +0200

    widget

---
+++ mathima 35/cascade_menu.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+
+def cut_function():
+    print("Cut function called")
+
+
+def copy_function():
+    print("Copy function called")
+
+
+def paste_function():
+    print("Paste function called")
+
+
+window = tk.Tk()
+window.title("Edit Menu Example")
+
+menu_bar = tk.Menu(window)
+
+edit_menu = tk.Menu(menu_bar, tearoff=0)
+edit_menu.add_command(label="Cut", command=cut_function)
+edit_menu.add_command(label="Copy", command=copy_function)
+edit_menu.add_command(label="Paste", command=paste_function)
+
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+
+window.config(menu=menu_bar)
+window.mainloop()
