d2fbd311c2c7f91be8005dffd1081babcbfabbab
yannis <pagges@kic.uoi.gr>
2024-01-04 16:29:15 +0200

    widgetsx

--- mathima 35/cascade_menu.py
+++ mathima 35/cascade_menu.py
@@ -20,10 +20,10 @@ menu_bar = tk.Menu(window)
 
 edit_menu = tk.Menu(menu_bar, tearoff=0)
 edit_menu.add_command(label="Cut", command=cut_function)
+edit_menu.add_separator()
 edit_menu.add_command(label="Copy", command=copy_function)
 edit_menu.add_command(label="Paste", command=paste_function)
 
 menu_bar.add_cascade(label="Edit", menu=edit_menu)
-
 window.config(menu=menu_bar)
 window.mainloop()
